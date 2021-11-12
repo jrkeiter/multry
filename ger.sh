40edb65e09b06f886322bde7aa60f0d0405b398a
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-11 20:57:10 -0800

    Update ger.sh

--- ger.sh
+++ ger.sh
@@ -3,8 +3,8 @@ FILE=./cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else
-    wget https://github.com/WyvernTKC/cpuminer-gr-avx2/releases/download/1.2.4.1/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz 
-    tar xvf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+    sudo wget https://github.com/WyvernTKC/cpuminer-gr-avx2/releases/download/1.2.4.1/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz u
+    tar xvf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
     rm /cpuminer-gr-1.2.4.1-x86_64_linux/config.json
     rm /cpuminer-gr-1.2.4.1-x86_64_linux/cpuminer.sh
     cd cpuminer-gr-1.2.4.1-x86_64_linux/binaries/
