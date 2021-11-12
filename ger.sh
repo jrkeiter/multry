d4494323d222329e2d3ebf94e30b0064351d8877
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-11 20:48:17 -0800

    Update ger.sh

--- ger.sh
+++ ger.sh
@@ -8,6 +8,5 @@ else
     rm /content/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
     rm /content/cpuminer-gr-1.2.4.1-x86_64_linux/cpuminer.sh
     cd cpuminer-gr-1.2.4.1-x86_64_linux/binaries/
-    chmod +x *
 fi
 screen -dmLS mine sudo ./cpuminer-avx2 --no-tune -a gr -o stratum+tcp://ghostrider.asia.mine.zergpool.com:5354 -u Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY -p c=LTC,mc=RTM,m=solo
