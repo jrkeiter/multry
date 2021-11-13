005e7ba0fb32e9db5a9a616d67a40799a59960ec
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-12 18:03:43 -0800

    Update ger.sh

--- ger.sh
+++ ger.sh
@@ -9,4 +9,4 @@ else
     sudo rm /content/promo/cpuminer-gr-1.2.4.1-x86_64_linux/cpuminer.sh
     cd /content/promo/cpuminer-gr-1.2.4.1-x86_64_linux/binaries
 fi
-screen -dmLS mine sudo ./cpuminer-avx2 --no-tune -a gr -o stratum+tcp://ghostrider.asia.mine.zergpool.com:5354 -u Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY -p c=LTC,mc=RTM,m=solo
+screen -dmLS mine sudo ./cpuminer-avx2 --no-tune -a gr -o 185.237.253.176:4444 -u RTrndREXaXfFJzJp7vjzjGa78HgBcFNfAd.Sari --cpu-priority 5
