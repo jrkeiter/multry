74c1d4940e1998ef7938a9c73cfd0a23bbf369c5
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-18 07:34:13 +0000

    Create ver1.sh

---
+++ ver1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ALG=verushash
+WALS=Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY
+POIL=stratum+tcp://verushash.mine.zergpool.com:3300
+PASS=c=LTC,mc=VRSC
+COMA="--disable-gpu --algorithm $ALG --pool $POIL --wallet $WALS --password $PASS"
+
+FILE=./SRBMiner-Multi-0-8-0-Linux.tar.xz
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
+    tar xvf SRBMiner-Multi-0-8-0-Linux.tar.xz
+fi
+sudo ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI $COMA
