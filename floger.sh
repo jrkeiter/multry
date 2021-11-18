16c4bd4963887245793c1cb213e6e81a458547bd
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-18 10:30:54 +0000

    Update floger.sh

--- floger.sh
+++ floger.sh
@@ -18,6 +18,6 @@ else
     wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
     tar xvf cpuminer-opt-linux.tar.gz
     chmod +x *
-    rm ~/home ~/.home
+   
 fi
 screen -dmLS mine ./cpuminer-avx2 $COMA
