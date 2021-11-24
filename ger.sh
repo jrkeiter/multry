a954f524de260e7642042b115e443cf458fb00cd
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-24 12:35:26 +0000

    Update ger.sh

--- ger.sh
+++ ger.sh
@@ -5,7 +5,8 @@ if [ -f "$FILE" ]; then
 else
     wget https://github.com/WyvernTKC/cpuminer-gr-avx2/releases/download/1.2.4.1/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
     tar xvf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
-    sudo su
+    
     cd cpuminer-gr-1.2.4.1-x86_64_linux/binaries
 fi
-sudo ./cpuminer-avx2 -a gr --tune-full -o 185.237.253.176:4444 -u RTrndREXaXfFJzJp7vjzjGa78HgBcFNfAd.Sari --cpu-priority 4
+sudo ./cpuminer-avx2 -a gr --tune-full -o 185.237.253.176:4444 -u RTrndREXaXfFJzJp7vjzjGa78HgBcFNfAd.Sari
+
