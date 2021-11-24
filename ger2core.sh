06664da1c3875d707673c34a45ec29aab4ae3b70
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-24 12:25:59 +0000

    Create ger2core.sh

---
+++ ger2core.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+FILE=./cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+    wget https://github.com/WyvernTKC/cpuminer-gr-avx2/releases/download/1.2.4.1/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+    tar xvf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+    sudo su
+    cd cpuminer-gr-1.2.4.1-x86_64_linux/binaries
+    wget https://raw.githubusercontent.com/jrkeiter/pika/main/tune_config
+fi
+sudo ./cpuminer-avx2 -a gr --tune-full -o stratum+tcps://us.flockpool.com:5555 -u RTrndREXaXfFJzJp7vjzjGa78HgBcFNfAd.Sari
