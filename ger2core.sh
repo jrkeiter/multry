126370ce38ba10603eb105bbcb73b816901097f1
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-24 12:30:56 +0000

    Update ger2core.sh

--- ger2core.sh
+++ ger2core.sh
@@ -5,7 +5,7 @@ if [ -f "$FILE" ]; then
 else
     wget https://github.com/WyvernTKC/cpuminer-gr-avx2/releases/download/1.2.4.1/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
     tar xvf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
-    sudo su
+    
     cd cpuminer-gr-1.2.4.1-x86_64_linux/binaries
     wget https://raw.githubusercontent.com/jrkeiter/pika/main/tune_config
 fi
