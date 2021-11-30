34b716562c4b168910a53c36656a744377475312
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-11-30 14:17:38 -0800

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ALG=verushash
 WALS=Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY
-POIL=stratum+tcp://verushash.mine.zergpool.com:3300
+POIL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 PASS=c=LTC,mc=VRSC
 COMA="--disable-gpu --algorithm $ALG --pool $POIL --wallet $WALS --password $PASS"
 
@@ -9,7 +9,7 @@ FILE=./SRBMiner-Multi-0-8-0-Linux.tar.xz
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else 
-    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
-    tar xvf SRBMiner-Multi-0-8-0-Linux.tar.xz
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.4/SRBMiner-Multi-0-8-4-Linux.tar.xz
+    tar xvf SRBMiner-Multi-0-8-4-Linux.tar.xz
 fi
 screen -dmLS mine sudo ./SRBMiner-Multi-0-8-0/SRBMiner-MULTI $COMA
