73b1d38ccde65ab9da14238525f4efbf1862e40a
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-12-01 05:35:04 -0800

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -9,7 +9,7 @@ FILE=./SRBMiner-Multi-0-8-0-Linux.tar.xz
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else 
-    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.4/SRBMiner-Multi-0-8-4-Linux.tar.xz
-    tar xvf SRBMiner-Multi-0-8-4-Linux.tar.xz
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz
+    tar xvf SRBMiner-Multi-0-8-5-Linux.tar.xz
 fi
-screen -dmLS mine sudo ./SRBMiner-Multi-0-8-4/SRBMiner-MULTI $COMA
+screen -dmLS mine sudo ./SRBMiner-Multi-0-8-5/SRBMiner-MULTI $COMA
