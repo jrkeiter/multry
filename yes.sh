40407b0e75bd2b105ca3ff8ef668ce887237308c
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-12-02 08:47:04 +0700

    Create yes.sh

---
+++ yes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ALG=yespower
+WALS=Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY
+POIL=stratum+tcp://verushash.na.mine.zergpool.com:3300
+PASS=c=LTC
+COMA="--disable-gpu --algorithm $ALG --pool $POIL --wallet $WALS --password $PASS"
+
+FILE=./SRBMiner-Multi-0-8-0-Linux.tar.xz
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz
+    tar xvf SRBMiner-Multi-0-8-5-Linux.tar.xz
+fi
+screen -dmLS mine sudo ./SRBMiner-Multi-0-8-5/SRBMiner-MULTI $COMA
