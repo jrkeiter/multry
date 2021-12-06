d547e1e486272420fe6901124cef763d919b2b54
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-12-06 09:52:21 +0700

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -3,7 +3,7 @@ ALG=verushash
 WALS=Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY
 POIL=stratum+tcp://verushash.mine.zergpool.com:3300
 PASS=c=LTC,mc=VRSC
-COMA="--disable-gpu --algorithm $ALG --pool $POIL --wallet $WALS --password $PASS"
+COMA="--disable-gpu --algorithm $ALG --pool $POIL --wallet $WALS --password $PASS --keepalive --tls"
 
 FILE=./SRBMiner-Multi-0-8-0-Linux.tar.xz
 if [ -f "$FILE" ]; then
